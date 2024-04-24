7a32eafc8b1d19436567e18e435edcde7fa68014
Biswajit s <biswajit.sen@thoughtworks.com>
2024-04-24 13:39:01 +0530

    Adds functions to derive equality

--- impls/bs/core.js
+++ impls/bs/core.js
@@ -3,6 +3,12 @@ const ns = {
   '-': (a, b) => a - b,
   '*': (a, b) => a * b,
   '/': (a, b) => a / b,
+  '=': (a, b) => a === b,
+  '<': (a, b) => a < b,
+  '<=': (a, b) => (a <= b),
+  '>': (a, b) => (a > b),
+  'and': (a, b) => (a && b),
+  '>=': (a, b) => (a >= b)
 };
 
 module.exports = ns;
